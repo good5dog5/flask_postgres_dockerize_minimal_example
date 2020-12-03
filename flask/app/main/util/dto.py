bfe6d4e690f48252bb0f3669e0aa54a652bdcd0c
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-12-03 11:28:22 +0800

    updates.

--- flask/app/main/util/dto.py
+++ flask/app/main/util/dto.py
@@ -3,23 +3,23 @@ from flask_restplus import Namespace, fields
 
 class UserDto:
     api = Namespace('user', description='user related operations')
-    # user = api.model('user',
-    #                  {'id': fields.Integer(readonly=True, description='user id'),
-    #                   'first_name': fields.String(required=True, description='user first name'),
-    #                   'job_title': fields.String(required=True, description='user job title'),
-    #                   'communication': {}
-    #                   })
-    #
-    # user['communication']['email'] = fields.String(required=True, description='user email address')
-    # user['communication']['mobile'] = fields.String(required=True, description='user mobile phone number')
+    user = api.model('user',
+                     {'id': fields.Integer(readonly=True, description='user id'),
+                      'first_name': fields.String(required=True, description='user first name'),
+                      'job_title': fields.String(required=True, description='user job title'),
+                      'communication': {}
+                      })
 
-    user = api.model('user', {
-        'id': fields.Integer(readonly=True, description='user id'),
-        'first_name': fields.String(required=True, description='user first name'),
-        'last_name': fields.String(required=True, description='user last name'),
-        'job_title': fields.String(required=True, description='user job title'),
-        'communication': fields.Nested(api.model('userCommunicationData', {
-            'email': fields.String(required=True, description='user email address'),
-            'mobile': fields.String(required=True, description='user mobile phone number'),
-        }))
-    })
+    user['communication']['email'] = fields.String(required=True, description='user email address')
+    user['communication']['mobile'] = fields.String(required=True, description='user mobile phone number')
+
+    # user = api.model('user', {
+    #     'id': fields.Integer(readonly=True, description='user id'),
+    #     'first_name': fields.String(required=True, description='user first name'),
+    #     'last_name': fields.String(required=True, description='user last name'),
+    #     'job_title': fields.String(required=True, description='user job title'),
+    #     'communication': fields.Nested(api.model('userCommunicationData', {
+    #         'email': fields.String(required=True, description='user email address'),
+    #         'mobile': fields.String(required=True, description='user mobile phone number'),
+    #     }))
+    # })
